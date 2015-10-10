b9151aa5caf48618d052738275f833153b71ae1d
Brandon Runyan <brando15@pilot.cis.ksu.edu>
2015-10-10 16:23:24 -0500

    sycall has basic structure, and writing/printing to the console works. Has no bad pointer checking

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static int getArgument(struct intr_frame *, int);
 
 void
 syscall_init (void) 
@@ -14,7 +15,75 @@ syscall_init (void)
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
+{ 
+  int syscall_num;
+  memcpy( &syscall_num, f->esp, sizeof(int));
+  switch(syscall_num)
+  { 
+    case(SYS_HALT):
+      shutdown_power_off();
+      break;
+    case(SYS_EXIT):
+      printf("exit call");
+      f->eax = 0;
+      thread_exit();
+      break;
+    case(SYS_EXEC):
+      printf("exec call");
+      thread_exit();
+      break;
+    case(SYS_WAIT):
+      printf("wait call");
+      thread_exit();
+      break;
+    case(SYS_CREATE):
+      printf("create call");
+      thread_exit();
+      break;
+    case(SYS_REMOVE):
+      printf("remove call");
+      thread_exit();
+      break;
+    case(SYS_OPEN):
+      printf("open call");
+      thread_exit();
+      break;
+    case(SYS_FILESIZE):
+      printf("filesize call");
+      thread_exit();
+      break;
+    case(SYS_READ):
+      printf("read call");
+      thread_exit();
+      break;
+    case(SYS_WRITE):
+      if(getArgument(f,1) == 1)
+		    putbuf(getArgument(f,2), getArgument(f,3)); 
+      break;
+    case(SYS_SEEK):
+      printf("seek call");
+      thread_exit();
+      break;
+    case(SYS_TELL):
+      printf("tell call");
+      thread_exit();
+      break;
+    case(SYS_CLOSE):
+      printf("close call");
+      thread_exit();
+      break;
+    default: 
+      printf("default, something went wrong");
+      thread_exit();
+  }
 }
+
+static int 
+getArgument(struct intr_frame *f, int index)
+{
+  int arg1;
+  memcpy( &arg1, ((int *)(f->esp))+index, sizeof(int));
+  return arg1;
+}
+  
+ 
